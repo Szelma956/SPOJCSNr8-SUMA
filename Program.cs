03d237d445195937679e442ee09135bf238f6cc6
Szelma956 <w.bielinski1@gmail.com>
2022-07-18 21:14:44 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SUMA
+{
+    class Sum
+    {
+        int memory = 0;
+
+        public void Add(int n)
+        {
+            memory = memory + n;
+            Console.WriteLine(memory);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "";
+            int number;
+            Sum sum = new Sum();
+
+            while (line != null)
+            {
+                line = Console.ReadLine();
+                if (line != null)
+                {
+                    number = int.Parse(line);
+                    sum.Add(number);
+                }
+            }
+
+        }
+    }
+}
